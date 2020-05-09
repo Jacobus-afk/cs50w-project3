6f8c4a0b6c5ce8d067596f8a8e0c7decbff3194e
Cobus <cobus@moduteq.com>
2020-05-09 19:47:49 +0200

    finished migrating pinochios data

--- importintodb.py
+++ importintodb.py
@@ -3,37 +3,105 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pizza.settings")
 import django
 django.setup()
-from orders.models import Order, Price, Product
+from orders.models import Order, Price, Product, Option
+from django.apps import apps
 pathlist = []
 namelist = []
-for file in os.listdir("./pinochio_data"):
-    if file.endswith(".csv"):
-        pathlist.append(os.path.join("./pinochio_data", file))
-        namelist.append(file.split(".")[0])
-for entry,name in zip(pathlist, namelist):
-    with open(entry) as csvf:
-        reader = csv.DictReader(csvf)
+productdict = {}
+orderdict = {}
+def get_paths(plist):
+    pathlist.clear()
+    namelist.clear()
+    for pathway in plist:
+        for file in os.listdir(pathway):
+            if file.endswith(".csv"):
+                pathlist.append(os.path.join(pathway, file))
+                namelist.append(file.split(".")[0])
+
+def populate_products():
+    for entry, name in zip(pathlist, namelist):
+        model_i_want = apps.get_model(app_label="orders", model_name=name)
+        #all_fields = model_i_want._meta.get_fields()
+        with open(entry) as csvf:
+            reader = csv.DictReader(csvf)
+            for row in reader:
+                try:
+                    m,_ = model_i_want.objects.get_or_create(**row)
+                    m_dict = m.__dict__
+                    productdict[m_dict['product']]=m_dict['id']
+
+                except Exception as e:
+                    print(e)
+        #model_i_want.objects.get_or_create()
+
+def populate_orders():
+    for entry in pathlist:
+        with open(entry) as csvf:
+            reader = csv.DictReader(csvf)
+            for row in reader:
+                p,_ = Product.objects.get_or_create(product=row["product"])
+                o,_ = Order.objects.get_or_create(product=p, size_choice=bool(int(row["size_choice"])), selection_limit=int(row["selection_limit"]), title=row["title"])
+                o_dict = o.__dict__
+                orderdict[o_dict['title']]=o_dict['id']
+
+def populate_options():
+    for entry, name in zip(pathlist, namelist):
         p,_ = Product.objects.get_or_create(product=name)
-        for row in reader:
-            pass
-            #p = Product(product = name)
-            
-            #p.save()
-            o,_ = Order.objects.get_or_create(product = p, title = row["title"], topping_code = row["topping_code"])
-            #o.save()
-            #print(o)
-            if "Normal" in row:
-                if row["Normal"]:
-                    pr,_ = Price.objects.get_or_create(order = o, price = row["Normal"], size = "NM" )
-                    #pr.save()
-                    print(pr)
-            if "Small" in row:
-                if row["Small"]:
-                    pr,_ = Price.objects.get_or_create(order = o, price = row["Small"], size = "SM" )
-                    #pr.save()
-                    print(pr)
-            if "Large" in row:
-                if row["Large"]:
-                    pr,_ = Price.objects.get_or_create(order = o, price = row["Large"], size = "LG" )
-                    #pr.save()
-                    print(pr)
+        with open(entry) as csvf:
+            reader = csv.DictReader(csvf)
+            for row in reader:
+                for order in Order.objects.filter(product=p):
+                    Option.objects.filter(order=order,selection_count=int(row["selection_count"])).update(option_name=row["option_name"])
+def populate_prices():
+    for entry in pathlist:
+        #p,_ = Product.objects.get_or_create(product=name)
+        with open(entry) as csvf:
+            reader = csv.DictReader(csvf)
+            for row in reader:
+                #product_id = productdict[row["product"]]
+                title_id = orderdict[row["title"]]
+                sel_cnt = int(row["selection_count"])
+                for o in Option.objects.filter(order_id=title_id, selection_count=sel_cnt):
+                    if row["Large"]:
+                        Price.objects.filter(option=o,size="LG").update(price=float(row["Large"]))
+                    if row["Small"]:
+                        Price.objects.filter(option=o,size="SM").update(price=float(row["Small"]))
+                    if row["Normal"]:
+                        Price.objects.filter(option=o,size="NM").update(price=float(row["Normal"]))
+
+if __name__ == "__main__":
+    #pathway = "./pinochio_data/order/"
+    #print("am here")
+    #from django.core import serializers
+
+    #data = serializers.serialize("json", [Product,Order,Option,Price] )
+
+    get_paths(["./pinochio_data/product/",])#"./pinochio_data/order/"])
+    populate_products()
+    get_paths(["./pinochio_data/order/",])
+    populate_orders()
+    get_paths(["./pinochio_data/option",])
+    populate_options()
+    get_paths(["./pinochio_data/price",])
+    populate_prices()
+# for entry,name in zip(pathlist, namelist):
+#     with open(entry) as csvf:
+#         reader = csv.DictReader(csvf)
+#         p,_ = Product.objects.get_or_create(product=name)
+#         for row in reader:
+#             o,_ = Order.objects.get_or_create(product = p, title = row["title"], selection_limit = row["topping_code"])
+#             for opt in Option.objects.filter(order=o):
+#                 print(opt)
+
+            # if "Normal" in row:
+            #     if row["Normal"]:
+            #         pr,_ = Price.objects.get_or_create(order = o, price = row["Normal"], size = "NM" )
+            #         print(pr)
+            # if "Small" in row:
+            #     if row["Small"]:
+            #         pr,_ = Price.objects.get_or_create(order = o, price = row["Small"], size = "SM" )
+            #         print(pr)
+            # if "Large" in row:
+            #     if row["Large"]:
+            #         pr,_ = Price.objects.get_or_create(order = o, price = row["Large"], size = "LG" )
+            #         print(pr)
--- orders/models.py
+++ orders/models.py
@@ -10,7 +10,7 @@ class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     title = models.CharField(max_length=32)
     selection_limit = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(4)])
-
+    size_choice = models.BooleanField(default=False)
     def __str__(self):
         return f"{self.product} - {self.title}"
     #https://stackoverflow.com/questions/12454829/django-auto-insert-rows-into-database
@@ -18,18 +18,31 @@ class Order(models.Model):
         is_new = self.pk is None
         super(Order, self).save(*args, **kwargs)
         if is_new:
-            print("Got here")
+            #print("Got here")
             for t in range(self.selection_limit+1):
-                cd = Option.objects.get_or_create(order=self,selection_count=t,option_name="Selection(s)")
+                cd = Option.objects.get_or_create(order=self, selection_count=t)
                 print(cd)
 
 class Option(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     selection_count = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(4)])
-    option_name = models.CharField(max_length=32)
+    option_name = models.CharField(max_length=32, default="Selection(s)")
 
     def __str__(self):
         return f"{self.order} #{self.selection_count} {self.option_name}"
+
+    def save(self, *args, **kwargs):
+        is_new = self.pk is None
+        super(Option, self).save(*args, **kwargs)
+        if is_new:
+            if (self.order.size_choice):
+                cd = Price.objects.get_or_create(option=self, size="LG")
+                print(cd)
+                cd = Price.objects.get_or_create(option=self, size="SM")
+                print(cd)
+            else:
+                cd = Price.objects.get_or_create(option=self)
+                print(cd)
 
 class Price(models.Model):
     SMALL = "SM"
@@ -41,7 +54,7 @@ class Price(models.Model):
         (NORMAL, "Normal")
     ]
     option = models.ForeignKey(Option, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=5, decimal_places=2, default=999.99)
     size = models.CharField(max_length=2, choices=SIZE_CHOICES, default=NORMAL)
 
     def __str__(self):
