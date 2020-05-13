4a90eee4c7a37af232ea348f3f83c1ea408a7ec1
Cobus <cobus@moduteq.com>
2020-05-13 23:18:18 +0200

    menu.html lists all current available products, options, prices

--- menu/urls.py
+++ menu/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="menu-home")
+    path("", views.home, name="menu-home"),
+    path("menu/", views.menu, name="menu-menu")
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
-
+from orders.models import Price, Product, Order, Option
 # Create your views here.
 def home(request):
     return render(request, 'menu/home.html')
+
+def menu(request):
+
+    prod_dict = {}
+    for prod in Product.objects.filter():
+        order_dict = {}
+        for order in Order.objects.filter(product=prod):
+            opt_dict = {}
+            for opt in Option.objects.filter(order=order):
+                price_dict = {}
+                for price in Price.objects.filter(option=opt):
+                    price_dict[price.size] = price.price
+                opt_dict[opt.option_name] = price_dict
+            order_dict[order.title] = opt_dict
+        prod_dict[prod.product] = order_dict
+
+        context = {
+        'products': prod_dict
+    }
+    return render(request, 'menu/menu.html', context)
