b61ef2f4e4bf64239958a47d501f006dc3809e56
Cobus <cobus@moduteq.com>
2020-05-17 19:19:46 +0200

    added option ajax req

--- menu/static/menu/menu.js
+++ menu/static/menu/menu.js
@@ -4,22 +4,74 @@ document.addEventListener("DOMContentLoaded", () => {
     const anchors = document.anchors;
     const menu_items = document.getElementById("menu-items");
     // products = JSON.parse(document.getElementById("products_var").textContent);
-    
+
     for (i = 0; i < anchors.length; i++) {
         anchors[i].addEventListener('click', (event) => {
             build_menu(event.currentTarget.name);
         });
     }
-        
-    function clear_menu()
-    {
+
+    function ajax_req(req_key, req, api_endpoint) {
+        return new Promise((res, rej) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open("POST", api_endpoint);
+            xhr.onload = () => {
+                // console.log('got here');
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    const data = JSON.parse(xhr.responseText);
+                    // console.log(data);
+                    res(data);
+                } else {
+                    rej({
+                        status: xhr.status,
+                        statusText: xhr.statusText
+                    });
+                }
+
+            }
+            const fdata = new FormData();
+            fdata.append(req_key, req);
+            fdata.append('csrfmiddlewaretoken', csrftoken);
+            xhr.send(fdata);
+        });
+    }
+
+    async function build_options(order_title, sel_lim) {
+        //console.log("got here - build_options "+ order_title );
+        const order_div = document.createElement("div");
+        order_div.className = "option-div";
+        let topping_count = 0;
+        try {
+            const data = await ajax_req("order_title", order_title, "/ajax/option");
+            console.log(data);
+            const options = data.options;
+            //const option_name = data.options.option_name;
+            //const sel_cnt = data.options.selection_count;
+            for (i in options) {
+                const option_name = data.options[i].option_name;
+                if (option_name.includes("Topping")) {
+                    topping_count++;
+                }
+                if (option_name.includes("Special")) {
+                    
+                }
+            }
+            
+            menu_items.append(order_div);
+        }
+        catch (err) {
+            console.log("Error:" + err);
+        }
+
+    }
+
+    function clear_menu() {
         while (menu_items.firstChild) {
             menu_items.removeChild(menu_items.lastChild);
         }
     }
 
     function build_orders(orders) {
-        // console.log(orders)
         const order_div = document.createElement("div");
         order_div.className = "order-div";
         const order_form = document.createElement("form");
@@ -33,15 +85,20 @@ document.addEventListener("DOMContentLoaded", () => {
             selection.name = "title";
             selection.value = title;
             selection.id = title;
+            selection.onclick = () => {
+                build_options(title, sel_lim);
+            };
             const label = document.createElement("label");
             label.htmlFor = title;
             label.innerHTML = title;
-            
 
-            order_form.append(selection);
-            order_form.append(label);
+
+            /*order_form*/order_div.append(selection);
+            /*order_form*/order_div.append(label);
+            
+            
             // console.log(sel_lim);
-            if (sel_lim > 0) {
+            /*if (sel_lim > 0) {
                 
                 const num = document.createElement("input");
                 num.type = "number";
@@ -49,7 +106,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 num.max = sel_lim;
                 order_form.append(num);
 
-            } 
+            } */
             order_form.append(br);
 
             //console.log(orders[i]);
@@ -58,11 +115,22 @@ document.addEventListener("DOMContentLoaded", () => {
         menu_items.append(order_div);
     }
 
-    function build_menu(product) {
+    async function build_menu(product) {
         clear_menu();
         // console.log(product)
+        try {
+            const data = await ajax_req("product", product, "/ajax/order")
+            // console.log(data);
+            build_orders(data.orders);
+        }
+        catch (err) {
+            console.log("Error: " + err);
+        }
 
-        const request = new XMLHttpRequest();
+
+
+
+        /*const request = new XMLHttpRequest();
         request.open("POST", "/ajax/order");
         request.onload = () => {
             const data = JSON.parse(request.responseText);
@@ -73,8 +141,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
         fdata.append("product", product);
         fdata.append('csrfmiddlewaretoken', csrftoken);
-        request.send(fdata);
-        
+        request.send(fdata);*/
+
         /*
         const orders = products[product];
 
--- menu/urls.py
+++ menu/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="menu-home"),
     path("ajax/order", views.post_order, name="menu-post-order"),
+    path("ajax/option", views.post_option, name="menu-post-option"),
     path("menu/", views.menu, name="menu-menu"),
 ]
